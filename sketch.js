ef47e0258f039e5956d59c046c33ea968b314d81
dikshasainani <53046878+dikshasainani@users.noreply.github.com>
2019-07-18 18:22:53 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -35,7 +35,7 @@ function preload(){
 function setup() {
   createCanvas(600, 200);
   
-  trex = createSprite(50,180,20,50);
+  trex = createSprite(50,180,21,50);
   
   trex.addAnimation("running", trex_running);
   trex.addAnimation("collided", trex_collided);
